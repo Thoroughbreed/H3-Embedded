4eabc2a032be1f3eb0b9f9387fdb95df6533caa5
Thoroughbreed <34303818+Thoroughbreed@users.noreply.github.com>
2021-05-11 13:39:32 +0200

    Create H3-Embedded-Skudaar-Prototyping-Header.h

---
+++ H3-Embedded-Skudaar-Prototyping-Header.h
@@ -0,0 +1,20 @@
+/*
+ * main.h
+ *
+ * Created: 11/05/2021 13.27.33
+ *  Author: jan
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define LED_Green 5
+#define LED_Red 7
+
+bool CalculateLeap(int y);
+
+
+
+
+#endif /* MAIN_H_ */
