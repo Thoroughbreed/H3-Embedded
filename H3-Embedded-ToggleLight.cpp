bd0841d6308672606869c8beb19adb0d1bd59f3b
Thoroughbreed <34303818+Thoroughbreed@users.noreply.github.com>
2021-05-11 14:57:26 +0200

    Create H3-Embedded-ToggleLight.cpp

---
+++ H3-Embedded-ToggleLight.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+#define KNAP 2
+#define LED 5
+bool KnapState;
+bool SidsteKnap = LOW;
+bool LedOn = false;
+
+unsigned long lastDebounce = 0;
+unsigned long debounceDelay = 50;
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(KNAP, INPUT_PULLUP);
+	pinMode(LED, OUTPUT);
+	digitalWrite(LED, LedOn);
+}
+
+void loop() {
+	bool KnapOn = digitalRead(KNAP);
+	if (KnapOn != SidsteKnap) {
+		lastDebounce = millis();
+	}
+	if ((millis() - lastDebounce) > debounceDelay) {
+		if (KnapOn != KnapState) {
+			KnapState = KnapOn;
+			if (KnapState == 1) LedOn = !LedOn;
+		}
+	}
+	
+	digitalWrite(LED, LedOn);
+	SidsteKnap = KnapOn;
+	
+}
