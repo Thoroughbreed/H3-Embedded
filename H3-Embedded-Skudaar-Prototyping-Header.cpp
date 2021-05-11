f6c2d5c62b63ac76b136c807d2b7b66595fee042
Thoroughbreed <34303818+Thoroughbreed@users.noreply.github.com>
2021-05-11 13:39:51 +0200

    Update H3-Embedded-Skudaar-Prototyping-Header.cpp

--- H3-Embedded-Skudaar-Prototyping-Header.cpp
+++ H3-Embedded-Skudaar-Prototyping-Header.cpp
@@ -1,1 +1,57 @@
+#include <Arduino.h>
+#include "main.h"
+//#define year 3600
 
+int year = 2000;
+int aarstal;
+
+void setup() {
+	Serial.begin(9600);
+	
+	pinMode(LED_Green, OUTPUT);
+	pinMode(LED_Red, OUTPUT);
+	digitalWrite(LED_Red, LOW);
+	digitalWrite(LED_Green, LOW);
+}
+
+void loop() {
+	if (CalculateLeap(year)) {
+		digitalWrite(LED_Green, HIGH);
+		digitalWrite(LED_Red, LOW);
+		Serial.print("Aaret ");
+		Serial.print(year);
+		Serial.println(" er et skudaar");
+	}
+	else {
+		digitalWrite(LED_Green, LOW);
+		digitalWrite(LED_Red, HIGH);
+		Serial.print("Aaret ");
+		Serial.print(year);
+		Serial.println(" er IKKE et skudaar");
+	}
+	
+	delay(500);
+	aarstal = Serial.parseInt(SKIP_ALL);
+	if (aarstal > 0 && aarstal != year) {
+	year = aarstal;
+	}
+}
+
+bool CalculateLeap(int y) {
+	if ((y % 4) == 0) {
+		if ((y % 100) == 0) {
+			if ((y % 400) == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return true;
+		}
+	}
+	else {
+		return false;
+	}
+}
