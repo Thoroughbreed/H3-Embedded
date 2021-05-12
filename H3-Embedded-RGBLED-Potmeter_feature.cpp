9a8c8641b6f46dc73d51ddffd44f6bffced1aff3
Thoroughbreed <34303818+Thoroughbreed@users.noreply.github.com>
2021-05-12 10:45:52 +0200

    Update H3-Embedded-RGBLED-Potmeter_feature.cpp
    Nu med printout KUN hvis værdien er ændret fra forrige potmeter indstilling

--- H3-Embedded-RGBLED-Potmeter_feature.cpp
+++ H3-Embedded-RGBLED-Potmeter_feature.cpp
@@ -8,6 +8,10 @@ int R_brightness = 0;
 int G_brightness = 0;
 int B_brightness = 0;
 
+int R_Old = 0;
+int G_Old = 0;
+int B_Old = 0;
+
 void setup() {
 	Serial.begin(9600);
 	pinMode(LED_R, OUTPUT);
@@ -24,12 +28,18 @@ void loop() {
 	analogWrite(LED_G, G_brightness);
 	analogWrite(LED_B, B_brightness);
 	
-	Serial.print("Red potmeter: ");
-	Serial.print(R_brightness);
-	Serial.print(" - Green potmeter: ");
-	Serial.print(G_brightness);
-	Serial.print(" - Blue potmeter: ");
-	Serial.print(B_brightness);
-	Serial.println();
+	if (R_brightness != R_Old || G_brightness != G_Old || B_brightness != B_Old)
+	{
+		Serial.print("Red potmeter: ");
+		Serial.print(R_brightness);
+		Serial.print(" - Green potmeter: ");
+		Serial.print(G_brightness);
+		Serial.print(" - Blue potmeter: ");
+		Serial.print(B_brightness);
+		Serial.println();		
+	}
 	delay(200);
+	R_Old = R_brightness;
+	G_Old = G_brightness;
+	B_Old = B_brightness;
 }
